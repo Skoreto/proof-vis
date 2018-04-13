61d5caf069722ad270c5c812cf7b40b8ccf3e231
Skoreto <TomSkorepa@gmail.com>
2018-04-13 21:43:19 +0200

    Add implementation of Edge hovering

--- src/components/Graph/Edge/Edge.js
+++ src/components/Graph/Edge/Edge.js
@@ -2,12 +2,34 @@ import React, {Component} from 'react';
 
 class Edge extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleMouseHover = this.handleMouseHover.bind(this);
+        this.state = {
+            isHovering: false,
+            strokeColor: props.strokeColor,
+            stroke: props.strokeColor
+        };
+    }
+
+    handleMouseHover() {
+        this.setState(this.changeFillHoverState);
+    }
+
+    changeFillHoverState(state) {
+        return {
+            isHovering: !state.isHovering,
+            stroke: state.isHovering ? state.strokeColor : 'lightblue'
+        };
+    }
+
     render() {
         const {x1, y1, x2, y2} = this.props;
 
         return (
             <svg>
-                <line x1={x1} y1={y1} x2={x2} y2={y2} stroke={'black'} strokeWidth={3} />
+                <line x1={x1} y1={y1} x2={x2} y2={y2} stroke={this.state.stroke} strokeWidth={3}
+                      onMouseEnter={this.handleMouseHover} onMouseLeave={this.handleMouseHover} />
             </svg>
         );
     }
--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -9,7 +9,7 @@ class Graph extends Component {
 
         return (
             <svg width={width} height={height}>
-                <Edge x1={90} y1={80} x2={290} y2={100} />
+                <Edge x1={90} y1={80} x2={290} y2={100} strokeColor={'black'} />
                 <Node cx={90} cy={80} fillColor={'yellow'} />
                 <Node cx={290} cy={100} fillColor={'red'} />
             </svg>
