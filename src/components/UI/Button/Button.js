4b46f74484993bfac8876082da10aa13d0c50753
Skoreto <TomSkorepa@gmail.com>
2018-04-15 19:48:07 +0200

    Eject and enable CSS Modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import cssClasses from './Button.css';
+
 const button = (props) => (
-    <button onClick={props.clicked}>{props.children}</button>
+    <button
+        className={cssClasses.Button}
+        onClick={props.clicked}>{props.children}</button>
 );
 
 export default button;
