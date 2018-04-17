1b119d02f3de3d76fe26d62df1b99a09ff24dcdc
Skoreto <TomSkorepa@gmail.com>
2018-04-17 18:43:32 +0200

    Render new Nodes in an array

--- src/App.js
+++ src/App.js
@@ -5,6 +5,32 @@ import Graph from './components/Graph/Graph';
 import Button from './components/UI/Button/Button'
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.nextStep = this.nextStep.bind(this);
+        this.state = {
+            nodes1: [
+                {x: 90, y: 280, fillColor: 'lightgreen' },
+                {x: 290, y: 240, fillColor: 'brown' }
+            ],
+            step: 0
+        };
+    }
+
+    nextStep = () => {
+        this.setState(this.changeNodesState);
+    }
+
+    changeNodesState = (state) => {
+        let newNodes = [...state.nodes1];
+        newNodes.push({x: 290, y: 300 + (state.step * 60), fillColor: 'green' });
+
+        return {
+            nodes1: newNodes,
+            step: ++state.step
+        };
+    }
+
     render() {
         return (
             <div className={cssClasses.App}>
@@ -17,10 +43,10 @@ class App extends Component {
                 </p>
 
                 <div className={cssClasses.GraphBox}>
-                    <Graph width={900} height={600} />
+                    <Graph width={900} height={600} nodes={this.state.nodes1} />
                 </div>
 
-                <Button>Další</Button>
+                <Button clicked={this.nextStep}>Další</Button>
             </div>
         );
     }
--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,29 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Node from './Node/Node';
 import Edge from './Edge/Edge';
 
-class Graph extends Component {
+const Graph = (props) => {
+    return (
+        <svg width={props.width} height={props.height} stroke={'black'} strokeWidth={2} >
+            <Edge x1={90} y1={80} x2={290} y2={100} strokeColor={'black'} />
+            <Node cx={90} cy={80} fillColor={'yellow'} label={'x'} />
+            <Node cx={290} cy={100} fillColor={'red'} label={'Y'} />
 
-    render() {
-        const {width, height} = this.props;
-
-        const nodes = [
-            {x: 90, y: 280, fillColor: 'lightgreen' },
-            {x: 290, y: 240, fillColor: 'brown' }
-        ];
-
-        return (
-            <svg width={width} height={height} stroke={'black'} strokeWidth={2} >
-                <Edge x1={90} y1={80} x2={290} y2={100} strokeColor={'black'} />
-                <Node cx={90} cy={80} fillColor={'yellow'} label={'x'} />
-                <Node cx={290} cy={100} fillColor={'red'} label={'Y'} />
-
-                {nodes.map((d, i) => {
-                    return <Node key={i} cx={d.x} cy={d.y} fillColor={d.fillColor} />
-                })}
-            </svg>
-        );
-    }
-}
+            {props.nodes.map((d, i) => {
+                return <Node key={i} cx={d.x} cy={d.y} fillColor={d.fillColor} />
+            })}
+        </svg>
+    );
+};
 
 export default Graph;
