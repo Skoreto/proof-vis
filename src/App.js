ec9c81eb0416a814115a24af85226c5cbf4affd6
Skoreto <TomSkorepa@gmail.com>
2018-05-01 19:46:54 +0200

    Rearrange layout, add favicon and logo

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import Exercise20vis from './components/exercises/Exercise20vis/Exercise20vis'
 import MainHeader from './components/UI/MainHeader/MainHeader'
 import CustomNavbar from './components/UI/CustomNavbar/CustomNavbar'
 import Footer from './components/UI/Footer/Footer'
+import BottomBar from './components/UI/Footer/BottomBar'
 
 class App extends Component {
     render() {
         return (
             <BrowserRouter basename={process.env.PUBLIC_URL}>
                 <div>
-                    <MainHeader/>
+                    {/*<MainHeader/>*/}
                     <CustomNavbar/>
                     <Route exact path="/" component={Exercise20} />
                     <Route path="/priklad20" component={Exercise20} />
                     <Route path="/priklad20vis" component={Exercise20vis} />
-                    <Footer/>
+                    {/*<Footer/>*/}
+                    <BottomBar/>
                 </div>
             </BrowserRouter>
         );
