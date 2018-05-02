df5ed5fe8563044e7d0b1db277f4688f2328f9d0
Skoreto <TomSkorepa@gmail.com>
2018-05-02 11:54:44 +0200

    Uninstall react-mathjax package, add MathJaxNode component

---
+++ src/components/MathJax/MathJaxNode.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MathJax from 'react-mathjax2';
+
+/**
+ * MathJax.Node wraper for shortening inline styling of TeX content.
+ * @returns {*}
+ * @constructor
+ */
+const MathJaxNode = (props) => (
+    <MathJax.Node inline>{props.children}</MathJax.Node>
+);
+
+export default MathJaxNode;
