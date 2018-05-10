c4896ab4ae71ed005c63ddcb904165f97123dc62
Skoreto <TomSkorepa@gmail.com>
2018-05-10 11:50:07 +0200

    Add editNode dialog (SingleDrawing)

--- src/components/pages/SingleDrawing/SingleDrawing.js
+++ src/components/pages/SingleDrawing/SingleDrawing.js
@@ -74,6 +74,15 @@ class SingleDrawing extends Component {
                         nodeData.shadow = shadow;
                         callback(nodeData);
                     },
+                    editNode: function (nodeData, callback) {
+                        // Predvyplneni dialog upravy vrcholu aktualnimi daty
+                        document.getElementById('inpNodeLabel').value = nodeData.label;
+                        document.getElementById('inpColorBackground').value = nodeData.color.background;
+                        document.getElementById('inpNodeSize').value = nodeData.size;
+                        // document.getElementById('btnSave').onclick = saveNode.bind(this, nodeData, callback);
+                        // document.getElementById('btnCancel').onclick = cancelNodeEdit.bind(this, callback);
+                        document.getElementById('editNodeDialog').style.display = 'block';
+                    },
                     editEdge: true,
                     deleteNode: true,
                     deleteEdge: true,
@@ -109,8 +118,41 @@ class SingleDrawing extends Component {
 
         return (
             <div>
-                <GraphVis graph={this.state.graphVis} options={this.state.options} events={events}
-                          style={{width: "100%", height: window.innerHeight - 120}} />
+                <div>
+                    <GraphVis graph={this.state.graphVis} options={this.state.options} events={events}
+                              style={{width: "100%", height: window.innerHeight - 120}} />
+                </div>
+                <div id="editNodeDialog">
+                    <span id="operation">Upravit vrchol</span>
+                    <br/>
+                    <br/>
+                    <form className="form-horizontal">
+                        <div className="form-group">
+                            <label htmlFor="inpNodeLabel" className="col-sm-2 control-label">Popisek</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control" id="inpNodeLabel"/>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="inpColorBackground" className="col-sm-2 control-label">Barva</label>
+                            <div className="col-sm-10">
+                                <input type="color" className="form-control" id="inpColorBackground"/>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="inpNodeSize" className="col-sm-2 control-label">Velikost</label>
+                            <div className="col-sm-10">
+                                <input type="number" className="form-control" id="inpNodeSize"/>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-12">
+                                <button type="button" className="btn btn-success" id="btnSave">Uložit</button>
+                                <button type="button" className="btn btn-default" id="btnCancel">Zrušit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
