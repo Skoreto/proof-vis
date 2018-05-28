a708c3cdf3201e356e1675c8c76367f2ac446312
Skoreto <TomSkorepa@gmail.com>
2018-05-28 17:55:43 +0200

    Skip over 'Eject and enable CSS Modules' commit 4b46f74484993bfac8876082da10aa13d0c50753

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
-import './App.css';
+import cssClasses from './App.css';
 import Graph from './components/Graph/Graph';
 import Button from './components/UI/Button/Button'
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to ProofVis</h1>
+            <div className={cssClasses.App}>
+                <header className={cssClasses["App-header"]}>
+                    <img src={logo} className={cssClasses["App-logo"]} alt="logo" />
+                    <h1 className={cssClasses["App-title"]}>Welcome to ProofVis</h1>
                 </header>
-                <p className="App-intro">
+                <p className={cssClasses["App-intro"]}>
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
 
-                <div className="GraphBox">
+                <div className={cssClasses.GraphBox}>
                     <Graph width={900} height={600} />
                 </div>
 
