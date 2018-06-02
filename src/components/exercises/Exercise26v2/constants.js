637f509aa9ae3ebf4850531eef71a8ac170c6521
Skoreto <TomSkorepa@gmail.com>
2018-06-02 21:30:41 +0200

    Update Exercise26v2 constants implementation

--- src/components/exercises/Exercise26v2/constants.js
+++ src/components/exercises/Exercise26v2/constants.js
@@ -13,35 +13,27 @@ export const definitionPanel = (
   </DefinitionPanel>
 );
 
-export function getProofBox(currentStep) {
+export const getProofBox = currentStep => {
   return (
-    <div className='bg-warning' id='proofBox'>
-      <div
-        className={
-          (1 === currentStep) || (2 === currentStep) 
-            ? 'proof-active' : ''
-        }
-      >
+    <div className="bg-warning" id="proofBox">
+      <div className={(1 === currentStep) || (2 === currentStep) ? "proof-active" : ""}>
         <p>
           Pokud graf <MN>G</MN> je strom
           <br /><MN>\Rightarrow</MN> pak dle věty o stromech platí, že pro každé dva vrcholy v grafu <MN>G</MN> existuje jediná cesta.
         </p>
       </div>
-      {/*<div className={2 === this.state.currentStep ? 'proof-active' : ''}>*/}
-      {/*<p>Mezi libovolně zvolenými vrcholy <MN>u</MN> a <MN>v</MN> existuje jediná cesta.</p>*/}
-      {/*</div>*/}
-      <div className={3 === currentStep ? 'proof-active' : ''}>
+      <div className={3 === currentStep ? "proof-active" : ""}>
         <p>
           <MN>\Rightarrow</MN> Protože existuje právě jediná cesta mezi vrcholy <MN>u,v</MN>, musí vždy vést přes libovolně zvolenou hranu <MN>{'e=\\{x,y\\}'}</MN> z této cesty.
         </p>
       </div>
-      <div className={4 === currentStep ? ' proof-active' : ''}>
+      <div className={4 === currentStep ? "proof-active" : ""}>
         <p>
           <MN>\Rightarrow</MN> Nicméně, v <MN>G-e</MN> neexistuje cesta <MN>u</MN>-<MN>v</MN>.
           <br /><MN>\Rightarrow</MN> Tudíž vrcholy <MN>u</MN>,<MN>v</MN> se v <MN>G-e</MN> nacházejí v různých komponentách souvislosti.
         </p>
       </div>
-      <div className={'borderless' + (5 === currentStep ? ' proof-active' : '')}>
+      <div className={'borderless' + (5 === currentStep ? " proof-active" : "")}>
         <p>
           <MN>\Rightarrow</MN> To znamená, že <MN>G-e</MN> není souvislý.
           <br /><MN>\Rightarrow</MN> Z definice stromu pak platí, že <MN>G-e</MN> není strom.
