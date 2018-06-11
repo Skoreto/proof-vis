28fe62e34b07d585e2adea488b993e3d0f776576
Skoreto <TomSkorepa@gmail.com>
2018-06-11 23:49:35 +0200

    Update Exercise 26 version 2 texts

--- src/components/exercises/Exercise26v2/constants.js
+++ src/components/exercises/Exercise26v2/constants.js
@@ -9,7 +9,7 @@ export const stepSum = 5;
 export const definitionPanel = (
   <DefinitionPanel>
     Dokažte přímo tvrzení: <cite><q>Jestliže graf <MN>G</MN> je strom, pak graf <MN>G-e</MN>, kde <MN>e</MN> je libovolná hrana grafu <MN>G</MN>, již není strom.</q></cite>
-    <br />(K důkazu použijte známé definice a věty týkající se stromů.)
+    <br />(Při dokazování použijte definice a věty z teorie týkající se stromů.)
   </DefinitionPanel>
 );
 
@@ -18,24 +18,24 @@ export const getProofBox = currentStep => {
     <div className="bg-warning" id="proofBox">
       <div className={(1 === currentStep) || (2 === currentStep) ? "proof-active" : ""}>
         <p>
-          Pokud graf <MN>G</MN> je strom
-          <br /><MN>\Rightarrow</MN> pak dle věty o stromech platí, že pro každé dva vrcholy v grafu <MN>G</MN> existuje jediná cesta.
+          Jestliže graf <MN>G</MN> je strom
+          <br /><MN>\Rightarrow</MN> pak podle věty o stromech platí, že pro každé dva vrcholy v grafu <MN>G</MN> existuje jediná cesta.
         </p>
       </div>
       <div className={3 === currentStep ? "proof-active" : ""}>
         <p>
-          <MN>\Rightarrow</MN> Protože existuje právě jediná cesta mezi vrcholy <MN>u,v</MN>, musí vždy vést přes libovolně zvolenou hranu <MN>{'e=\\{x,y\\}'}</MN> z této cesty.
+          <MN>\Rightarrow</MN> Protože existuje právě jedna cesta mezi vrcholy <MN>u,v</MN>, musí vždy vést přes libovolně zvolenou hranu <MN>{'e=\\{x,y\\}'}</MN> této cesty.
         </p>
       </div>
       <div className={4 === currentStep ? "proof-active" : ""}>
         <p>
           <MN>\Rightarrow</MN> Nicméně, v <MN>G-e</MN> neexistuje cesta <MN>u</MN>-<MN>v</MN>.
-          <br /><MN>\Rightarrow</MN> Tudíž vrcholy <MN>u</MN>,<MN>v</MN> se v <MN>G-e</MN> nacházejí v různých komponentách souvislosti.
+          <br /><MN>\Rightarrow</MN> Proto se vrcholy <MN>u</MN>,<MN>v</MN> v <MN>G-e</MN> nacházejí v různých komponentách souvislosti.
         </p>
       </div>
       <div className={'borderless' + (5 === currentStep ? " proof-active" : "")}>
         <p>
-          <MN>\Rightarrow</MN> To znamená, že <MN>G-e</MN> není souvislý.
+          <MN>\Rightarrow</MN> Z toho plyne, že <MN>G-e</MN> není souvislý.
           <br /><MN>\Rightarrow</MN> Z definice stromu pak platí, že <MN>G-e</MN> není strom.
         </p>
         <p className="text-center">
