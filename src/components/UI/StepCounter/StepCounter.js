c6889d96d6d7290a9ddd9abf89ad7a86fa35e4f8
Skoreto <TomSkorepa@gmail.com>
2018-07-21 12:09:02 +0200

    Update layout of StepCounted component

--- src/components/UI/StepCounter/StepCounter.js
+++ src/components/UI/StepCounter/StepCounter.js
@@ -2,10 +2,8 @@ import React from 'react';
 import './StepCounter.css';
 
 const StepCounter = (props) => (
-  <span className={'step-counter'}>
-    <span>{props.currentStep}</span>
-    <span>/</span>
-    <span>{props.stepSum}</span>
+  <span className="step-counter">
+    {props.currentStep}/{props.stepSum}
   </span>
 );
 
