fa3d99cf79e9ebc903d627c128ddfd1268b280e2
Skoreto <TomSkorepa@gmail.com>
2018-08-03 15:07:38 +0200

    Create separate CSS file for ClaimPanel component

--- src/UI/ExerciseWrapper/ClaimPanel/ClaimPanel.jsx
+++ src/UI/ExerciseWrapper/ClaimPanel/ClaimPanel.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Row, Col } from 'react-bootstrap';
 import MathJax from 'react-mathjax2';
+import './ClaimPanel.css';
 
 const ClaimPanel = (props) => (
   <Row className="page-row">
     <Col xs={12} md={12} lg={12}>
       <MathJax.Context input="tex">
-        <div className="bg-info" id="definition">
+        <div className="claim-panel">
           {props.children}
         </div>
       </MathJax.Context>
